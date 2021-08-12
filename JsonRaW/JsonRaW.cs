f7885b33c14cd51107b73ca3911fa24f15a5c6c4
SeyedAli <SeyedAli@DESKTOP-D2DHOLQ>
2021-08-12 17:33:51 +0430

    Add project files.

---
+++ JsonRaW/JsonRaW.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+namespace JsonRaW
+{
+    public class JsonRaW<T>
+    {
+        /// <summary>
+        /// Reads all json text from a json file and Deserialize it in type List<T>/>
+        /// </summary>
+        /// <param name="jsonFilePath">Json file path</param>
+        /// <returns>Returns List of type T read from a json file</returns>
+        public static List<T> jsonReader(string jsonFilePath)
+        {
+            List<T> jsonObject = null;
+            string json = File.ReadAllText(jsonFilePath);
+            jsonObject =JsonSerializer.Deserialize<List<T>>(json);
+            return jsonObject ?? new List<T>();
+        }
+        /// <summary>
+        /// Writes an object from type List<T> in a file in path "JsonFilePath" 
+        /// </summary>
+        /// <param name="convertibleObject">an object from type List<T></param>
+        /// <param name="jsonFilePath">Json file path</param>
+        public static void jsonWriter(List<T> convertibleObject, string jsonFilePath)
+        {
+            JsonFile.checkDirectory(jsonFilePath);
+            string convertedJson;
+            convertedJson = JsonSerializer.Serialize(convertibleObject); 
+            File.WriteAllText(jsonFilePath, convertedJson);
+        }
+        
+    }
+    public class JsonFile
+    {
+        /// <summary>
+        /// Checks JsonFilePath and if this file not exist, create it. 
+        ///JsonRaW.JsonReader() and JsonRaW.JsonWriter() contains this methods
+        /// </summary>
+        /// <param name="jsonFilePath">Json file path</param>
+        public static void checkFile(string jsonFilePath)
+        {
+            checkDirectory(jsonFilePath);
+            if (File.Exists(jsonFilePath) == false)
+            {
+                File.WriteAllText(jsonFilePath, null);
+            }
+        }
+        internal static void checkDirectory(string jsonFilePath)
+        {
+            if (Directory.Exists(Path.GetDirectoryName(jsonFilePath)) == false)
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(jsonFilePath));
+            }
+        }
+    }
+}
