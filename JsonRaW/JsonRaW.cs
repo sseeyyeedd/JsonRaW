86cad0f9f38ebd75fab7ce610a880e616f99f57a
SeyedAli Hoseini <seyyedalihoseini@hotmail.com>
2021-10-05 19:29:43 +0330

    version 2.0.0

--- JsonRaW/JsonRaW.cs
+++ JsonRaW/JsonRaW.cs
@@ -2,28 +2,42 @@
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
+using System.Net.Http;
 namespace JsonRaW
 {
     public class JsonRaW<T>
     {
         /// <summary>
-        /// Reads all json text from a json file and Deserialize it in type List<T>/>
+        /// Reads all json text from a json file and Deserialize it in type IEnumerable<T>/>
         /// </summary>
         /// <param name="jsonFilePath">Json file path</param>
-        /// <returns>Returns List of type T read from a json file</returns>
-        public static List<T> jsonReader(string jsonFilePath)
+        /// <returns>Returns IEnumerable of type T read from a json file</returns>
+        public static IEnumerable<T> jsonReader(string jsonFilePath)
         {
-            List<T> jsonObject = null;
+            IEnumerable<T> jsonObject = null;
             string json = File.ReadAllText(jsonFilePath);
             jsonObject =JsonConvert.DeserializeObject<List<T>>(json);
             return jsonObject ?? new List<T>();
         }
         /// <summary>
-        /// Writes an object from type List<T> in a file in path "JsonFilePath" 
+        /// Reads all json text from a web address and Deserialize it in type IEnumerable<T>
         /// </summary>
-        /// <param name="convertibleObject">an object from type List<T></param>
+        /// <param name="jsonWebAddress">Json web address</param>
+        /// <returns>Returns IEnumerable of type T read from a web address</returns>
+        public static IEnumerable<T> JsonReader(string jsonWebAddress)
+        {
+            IEnumerable<T> jsonObject = null;
+            HttpClient client = new HttpClient();
+            var response=client.GetAsync(jsonWebAddress);
+            jsonObject = JsonConvert.DeserializeObject(response.Result.Content.ToString()) as IEnumerable<T>;
+            return jsonObject ?? new List<T>();
+        }
+        /// <summary>
+        /// Writes an object from type IEnumerable<T> in a file in path "JsonFilePath" 
+        /// </summary>
+        /// <param name="convertibleObject">an object from type IEnumerable<T></param>
         /// <param name="jsonFilePath">Json file path</param>
-        public static void jsonWriter(List<T> convertibleObject, string jsonFilePath)
+        public static void jsonWriter(IEnumerable<T> convertibleObject, string jsonFilePath)
         {
             JsonFile.checkDirectory(jsonFilePath);
             string convertedJson;
@@ -31,6 +45,7 @@ namespace JsonRaW
             File.WriteAllText(jsonFilePath, convertedJson);
         }
         
+
     }
     public class JsonFile
     {
